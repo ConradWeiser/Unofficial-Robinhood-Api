a66941c55a6f7a88f8802a4bf810170b9731c418
VibrantClouds <bluecrecent0@gmail.com>
2017-04-21 17:52:37 -0400

    Configuration Manager + Log Handler

---
+++ src/main/java/robinhood/api/ConfigurationManager.java
@@ -0,0 +1,55 @@
+package robinhood.api;
+
+import robinhood.api.throwables.TokenNotFoundException;
+
+/**
+ * Method which stores the current configuration for the library.
+ * The authentication key used for most of the other functions is stored here.
+ * 
+ * Singleton is used so there can only be one instance of this running.
+ * @author Conrad
+ *
+ */
+public class ConfigurationManager {
+	
+	/**
+	 * The authentication token for the logged in user, if one exists
+	 */
+	private String authToken = null;
+	
+	/**
+	 * The current instance of the ConfigurationManager
+	 */
+	private static ConfigurationManager instance = null;
+	
+	/**
+	 * Method which gets the current instance of the ConfigurationManager
+	 * If one does not exist, it creates one and returns it
+	 */
+	public static ConfigurationManager getInstance() {
+		
+		if(ConfigurationManager.instance == null) {
+			ConfigurationManager.instance = new ConfigurationManager();
+		}
+		
+		return ConfigurationManager.instance;
+	}
+	
+	/**
+	 * Method which gets the saved authorization token if the user is logged in.
+	 * If one does not exist, it throws an error reminding the user to run the login functions
+	 * first.
+	 * 
+	 * @return the saved Token for the logged in user
+	 * @throws TokenNotFoundException
+	 */
+	public String getToken() throws TokenNotFoundException {
+		
+		if(authToken == null)
+			throw new TokenNotFoundException();
+		
+		return this.authToken;
+	}
+	
+
+}
---
+++ src/main/java/robinhood/api/throwables/TokenNotFoundException.java
@@ -0,0 +1,17 @@
+package robinhood.api.throwables;
+
+@SuppressWarnings("serial")
+public class TokenNotFoundException extends Exception {
+	
+	/**
+	 * Override the default method to get more information about
+	 * why the error is thrown
+	 * 
+	 * @return more information about the exception
+	 */
+	
+	@Override public String getMessage() {
+		return "There is no Token available. Make sure that the login method is being used first.";
+	}
+
+}
